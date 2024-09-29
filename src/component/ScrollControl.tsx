16a959f7e25867e550f18bf2a06dd39844b63088
Shark (Yu) <yusoon.97417@gmail.com>
2024-09-29 11:14:11 +0800

    style: change word into icon

--- src/component/ScrollControl.tsx
+++ src/component/ScrollControl.tsx
@@ -1,19 +1,22 @@
-const ScrollControl: React.FC<{ autoScroll: boolean; toggleAutoScroll: () => void }> = ({
-    autoScroll,
-    toggleAutoScroll,
-  }) => {
-    return (
-      <div className="flex flex-col">
-        <div>Auto Scroll</div>
-        <button
-          onClick={toggleAutoScroll}
-          className={`mt-2 px-4 py-2 ${autoScroll ? "bg-green-500" : "bg-gray-500"} text-white rounded`}
-        >
-          {autoScroll ? "Disable" : "Enable"}
-        </button>
-      </div>
-    );
-  };
-  
-  export default ScrollControl;
-  
+import { MouseOff, Mouse } from "lucide-react";
+
+const ScrollControl: React.FC<{
+  autoScroll: boolean;
+  toggleAutoScroll: () => void;
+}> = ({ autoScroll, toggleAutoScroll }) => {
+  return (
+    <div className="flex flex-col">
+      <div>Auto Scroll</div>
+      <button
+        onClick={toggleAutoScroll}
+        className={`mt-2 px-4 py-2 mx-auto ${
+          autoScroll ? "bg-green-500" : "bg-gray-500"
+        } text-white rounded`}
+      >
+        {autoScroll ? <Mouse /> : <MouseOff />}
+      </button>
+    </div>
+  );
+};
+
+export default ScrollControl;
